9e13551a47183f4aa1cd37aaef2660209aef2c68
LDZoid <lucijan345@gmail.com>
2021-08-01 18:29:12 +0200

    finished basic functionality no error handlings

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const btnClear = document.querySelector("#btn-clear")
 const btnAdd = document.querySelector("#btn-add")
 
 // else
-const booksContainer = document.querySelector("#books-container")
+const booksContainer = document.querySelector(".books-container")
 
 
 // BTNS
@@ -37,9 +37,41 @@ btnAdd.addEventListener("click", () => {
     let book = document.createElement("div")
     book.classList.add("book")
     booksContainer.appendChild(book)
-    let bookTitle = document.createElement("h3")
-    let bookAuthor = document.createElement("h4")
+    // add title and author
+    let bookTitle = document.createElement("h1")
+    let bookAuthor = document.createElement("h2")
+    bookTitle.innerHTML = inputTitle.value
+    bookAuthor.innerHTML = inputAuthor.value
+    book.appendChild(bookTitle)
+    book.appendChild(bookAuthor)
+    // add pages and del/edit option
+    let optionsContainer = document.createElement("div")
+    optionsContainer.classList.add("options-container")
+    let optionEditContainer = document.createElement("div")
+    optionEditContainer.classList.add("options-edit-container")
+    let optionDelete = document.createElement("button")
+    optionDelete.classList.add("option-delete")
+    let optionEditDisplay = document.createElement("div")
+    optionEditDisplay.classList.add("edit-display")
+    let optionEditPlus = document.createElement("button")
+    optionEditPlus.classList.add("edit-pages")
+    optionEditPlus.classList.add("plus")
+    let optionEditMinus = document.createElement("button")
+    optionEditMinus.classList.add("edit-pages")
+    optionEditMinus.classList.add("minus")
+    book.appendChild(optionsContainer)
+    optionsContainer.appendChild(optionEditContainer)
+    optionsContainer.appendChild(optionDelete)
+    optionEditContainer.appendChild(optionEditDisplay)
+    optionEditContainer.appendChild(optionEditPlus)
+    optionEditContainer.appendChild(optionEditMinus)
+    optionEditDisplay.innerHTML = `<span class="span-pages-read">${inputPagesRead.value}</span>/<span class="span-pages-total">${inputPagesTotal.value}</span>`
+    optionEditMinus.innerHTML = "-"
+    optionEditPlus.innerHTML = "+"
+    optionDelete.innerHTML = "delete"
+    updateEventListeners()
     // 3.update info
+    updateInfo()
 })
 
 
@@ -48,5 +80,51 @@ btnAdd.addEventListener("click", () => {
 
 // loops through all books and sets information about books and pages on sidebar
 function updateInfo() {
+    // count total books
+    totalBooks.innerHTML = document.querySelectorAll(".book").length
+    // count read pages
+    let pagesReadTemp = 0
+    document.querySelectorAll(".span-pages-read").forEach(i => pagesReadTemp = pagesReadTemp + parseInt(i.innerHTML))
+    pagesRead.innerHTML = pagesReadTemp
+    // count total pages
+    let pagesTotalTemp = 0
+    document.querySelectorAll(".span-pages-total").forEach(i => pagesTotalTemp = pagesTotalTemp + parseInt(i.innerHTML))
+    totalPages.innerHTML = pagesTotalTemp
+    // count read books
+    let readBooksCount = 0
+    document.querySelectorAll(".book").forEach(book => {
+        let left = book.querySelector(".span-pages-read").innerHTML
+        let right = book.querySelector(".span-pages-total").innerHTML
+        if (left == right) {
+            readBooksCount += 1
+        }
+    })
+    booksRead.innerHTML = readBooksCount
+}
 
+function updateEventListeners() {
+    // delete btn 
+    let btnsDelete = document.querySelectorAll(".option-delete")
+    btnsDelete.forEach(btn => {
+        btn.addEventListener("click", () => {
+            btn.parentElement.parentElement.remove()
+            updateInfo()
+        })
+    })
+    // +/- btns
+    let btnsIncDec = document.querySelectorAll(".edit-pages")
+    btnsIncDec.forEach(btn => {
+        btn.addEventListener("click", () => {
+            let pagesReadSpan = btn.parentElement.querySelector(".span-pages-read")
+            switch (btn.innerHTML) {
+                case "+":
+                    pagesReadSpan.innerHTML = parseInt(pagesReadSpan.innerHTML) + 1
+                    break
+                case "-":
+                    pagesReadSpan.innerHTML = parseInt(pagesReadSpan.innerHTML) - 1
+                    break
+            }
+            updateInfo()
+        })
+    })
 }
